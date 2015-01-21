ffdf9e2b0ca4dd8451df802d31388c699ba5f02b
NickTaporuk <nictaporuk@yandex.ru>
2015-01-21 09:19:57 +0200

    add new controller

---
+++ application/views/znakomster/index.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<div class="header"></div>
+<div class="body">Hello</div>
+<div class="footer"></div>
+</body>
+</html>
