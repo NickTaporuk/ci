7c9a7b06ede5f418b42590142ef8be8cf5f69b20
NickTaporuk <nictaporuk@yandex.ru>
2015-01-23 14:38:22 +0200

    add func users model

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -1,40 +1,58 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Users_model extends CI_Model {
+class Users_model extends CI_Model
+{
     function __construct()
     {
         // Call the Model constructor
         parent::__construct();
-//        echo 111;
         $this->load->database();
-//        var_dump($this->load->database());
     }
 
     /**
      * @return mixed
      */
-    public function get_users(){
+    public function get_users()
+    {
         $query = $this->db->get('users');
         return $query->result_array();
     }
+
     /**
      * @return mixed
      */
-    public function get_user($id){
-        $this->db->limit('1') ;
-        $this->db->order_by('id') ;
+    public function get_user($id)
+    {
+        $this->db->limit('1');
+        $this->db->order_by('id');
         $query = $this->db->get('users');
         return $query->result_array();
     }
+
     /**
      * @return mixed
      */
-    public function insert_user(array $user){
-        $this->db->insert('users',$user) ;
+    public function insert_user(array $user)
+    {
+        $this->db->insert('users', $user);
         return true;
     }
 
-    public function edit_user(array $user) {
+    public function edit_user(array $user)
+    {
+        try {
+            if ($user['id']) {
+                $this->db->where('id', $user['id']);
+                unset($user['id']);
+                $this->db->update('users', $user);
+            } else {
+                $error  = 'No id in array IN FUNCTION :'.__FUNCTION__.' LINE :'.__LINE__;
+                throw new Exception($error);
+            }
+
+        } catch (Exception $e) {
+            error_log(print_r([$user,$error]));
+        }
 
     }
 
